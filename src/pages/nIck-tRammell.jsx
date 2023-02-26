adcc3e83c663855a2cf75985b349ddfc98304de6
Emmanuel Dick <emmanueldick@Emmanuels-MacBook-Pro.local>
2023-02-26 01:00:11 -0500

    Authour page update

--- src/pages/nIck-tRammell.jsx
+++ src/pages/nIck-tRammell.jsx
@@ -7,7 +7,7 @@ const Author = () => {
         <MDBTypography tag='div' className='display-6 pb-3 mt-5 mr-auto' >
         nIck-tRammell
         </MDBTypography>
-        <MDBTypography tag='p' className='lead pb-3 mb-10 mr-auto text-center' >
+        <MDBTypography tag='p' className='lead pb-3 mb-10 mr-auto' >
           Originally from rural South Carolina, nIck tRammell has transformed the adult erotica genre for the better. Starting with his acclaimed UNCLE MIKE series!
           Uncle Mike is just one of many erotic short story series penned by nIck tRammell. Many tales are pure fantasy– a lot are transcripts of actual encounters. Each story guaranteed better then the last!
         </MDBTypography>
