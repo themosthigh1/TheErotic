ca9e40dd35c602dc697b12ee08bb6d5f4cca535d
Emmanuel Dick <emmanueldick@Emmanuels-MacBook-Pro.local>
2023-02-26 04:30:55 -0500

    updated Head tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import BookCards from "@/components/books/BookCards";
 import Header from "@/components/Header";
 import Section from "@/components/Section";
+import Head from "next/head";
+
 
 
 
@@ -8,6 +10,12 @@ import Section from "@/components/Section";
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Erotic Storyteller</title>
+        <meta name="description" content="nIck tRammell" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header/>
       <BookCards/>
       <Section />
